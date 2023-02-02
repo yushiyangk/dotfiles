dedf5449c114be4501cd87a3cf1c7d79f777ffa3
Yu Shiyang <yu.shiyang@gnayihs.uy>
2023-02-03 01:50:49 +0800

    Made sync_dotfiles.sh more robust for bash

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -9,6 +9,18 @@
 git pull --ff-only && ./install_dotfiles.sh
 
 # Check if bash
-if [ -n "$BASH" ]; then
-	source "$HOME/.bash_profile"
+if [ -n "$BASH" ] || [ -n "$BASH_VERSION"]; then
+	profiled=0
+	if [ "$profiled" -eq 0 ] && [ -x "$HOME/.bash_profile" ]; then
+		source "$HOME/.bash_profile"
+		profiled=1
+	fi
+	if [ "$profiled" -eq 0 ] && [ -x "$HOME/.bash_login" ]; then
+		source "$HOME/.bash_login"
+		profiled=1
+	fi
+	if [ "$profiled" -eq 0 ] && [ -x "$HOME/.profile" ]; then
+		source "$HOME/.profile"
+		profiled=1
+	fi
 fi
