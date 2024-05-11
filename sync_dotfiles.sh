ba505cd6c46dc0060d0e8bf001b9af9bdded669d
Yu Shiyang <Yu Shiyang>
2024-05-12 07:09:44 +0800

    Help message and options for sync_dotfiles.sh
    All existing options are passed on to install_files.sh, but that need not necessarily the case for other future options.

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -5,8 +5,81 @@
 #
 # Yu Shiyang <yu.shiyang@gnayihs.uy>
 
+
+name='sync_dotfiles.sh'
+
+help="Usage: $name [-h] [<options>] [<target>]"
+help="$help\nPull the latest version of dotfiles from the remote repository and install them"
+help="$help\n"
+help="$help\nParameters:"
+help="$help\n  <target>        Target label that describes the local system, which also"
+help="$help\n                  corresponds to the subdirectory of the same name;"
+help="$help\n                  default: posix"
+help="$help\n"
+help="$help\nOptions:"
+help="$help\n  -h, -?, --help  Display this help message and exit"
+
+
+target='posix'
+quiet=0
+
+argi=0
+while [ $# -gt 0 ]; do
+	arg="$1"
+	case "$arg" in
+		--help)
+			echo -e "$help"
+			exit 0
+			;;
+		--quiet)
+			((++quiet))
+			;;
+		--*)
+			echo "$name: invalid option '$arg'" >& 2
+			exit 2
+			;;
+		-*)
+			for i in $(seq 1 $((${#arg} - 1))); do
+				flag="${arg:$i:1}"
+				case "$flag" in
+					h|\?)
+						echo -e "$help"
+						exit 0
+						;;
+					q)
+						((++quiet))
+						;;
+					*)
+						echo "$name: invalid option '$flag'" >& 2
+						exit 2
+				esac
+			done
+			;;
+		*)
+			case "$argi" in
+				0)
+					target="$arg"
+					;;
+				*)
+					echo "$name: unexpected parameter '$arg'" >& 2
+					exit 2
+					;;
+			esac
+			((++argi))
+	esac
+	shift
+done
+
+
+install_args=("$target")
+if [ $quiet -ne 0 ]; then
+	install_args+=("-$( printf 'q%.0s' {1..$quiet} )")
+	# {1..$quiet} expands to integers 1 through $quiet
+	# 'q%.0s' turns each into just 'q' because the '%.0s' specifies a string of maximum length 0
+fi
+
 # Update from git repository and install
-git pull --ff-only && ./install_dotfiles.sh
+git pull --ff-only && ./install_dotfiles.sh ${install_args[@]}
 
 # Check if bash
 if [ -n "$BASH" ] || [ -n "$BASH_VERSION"]; then
